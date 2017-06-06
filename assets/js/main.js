cf94fb47e77bc25d3fb3d2e0bcb4b17ee8e50b4a
Pola <Pola.kinomoto@gmail.com>
2017-06-06 15:41:54 -0400

    completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 /*funcion del click*/
-var botonStart = document.getElementsByClassName("bton")[0];
-botonStart.addEventListener("click", test);
+function boton(){
+	var lista = document.getElementById("chao");
+	lista.setAttribute("class", "new");
+
+    var elemento = Array.from(document.getElementsByClassName("chao"));
+      elemento.forEach(function(el){
+    el.setAttribute("class", "new");
+  });
+}
+ 
+ function test(){
+ 	var botonStart = document.getElementById("hola");
+	botonStart.addEventListener("click", boton);
+}
+ test();
